6b55fcd321ffd1d557817d1ad188e940cd2d7c56
Lucas Santos <lucas1295santos@gmail.com>
2020-01-20 20:56:22 -0300

    Creating a test target to the app

---
+++ appetize_test/appetize_testTests/appetize_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  appetize_testTests.swift
+//  appetize_testTests
+//
+//  Created by Lucas Oliveira on 20/01/20.
+//  Copyright © 2020 Lucas Oliveira. All rights reserved.
+//
+
+import XCTest
+
+class appetize_testTests: XCTestCase {
+
+    func test_nothing() {
+        XCTAssertTrue(true)
+    }
+
+}
